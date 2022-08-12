9958045014d35ee4b8c88469e3e92fb64c8bb74b
dowdyp <monorail65@gmail.com>
2022-08-12 00:26:56 -0400

    change config

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -18,11 +18,6 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
